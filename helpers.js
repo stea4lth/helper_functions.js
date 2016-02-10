39ad70a89772f4ca6d7a20ea017843c606ddc11d
Chris <chrisnowlan321@gmail.com>
2016-02-10 13:18:44 -0500

    Update helpers.js

--- helpers.js
+++ helpers.js
@@ -1,4 +1,12 @@
 
+// prototypes
+String.prototype.ucFirst = function() {
+	return this.charAt(0).toUpperCase() + this.slice(1);
+}
+String.prototype.trunc = function(n) {
+	return this.substr(0, n - 1) + (this.length > n ? '...' : '');
+};
+
 /**
  * Recursively copies one directory to another.
  *
